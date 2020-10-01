1d4f8ea24ad3d054bb0ae754a8513e71f72e1b84
Kevin Schmidt <schmidtk14@gmail.com>
2020-10-01 10:16:41 -0400

    Login page added and aws amplify used to login to the backend with credentials
    -App.js updated with logout button which replaces login and signup if user is
     authenticated
    -App.js now wraps </routes> with AppContext which utilizes react context to
     all user auth context to be passed through app
    -libs/contextLib.js added to initialize context for passing user information
     throughout app
    -config.js added to contain necessary information to connect with AWS backend
    -index.js updated to initialze AWS using information from config.js

--- src/App.js
+++ src/App.js
@@ -1,32 +1,70 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import Routes from "./Routes";
 import { Link } from "react-router-dom";
 import { Nav, Navbar, NavItem } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
+import { AppContext } from "./libs/contextLib";
+import { Auth } from "aws-amplify";
+
 
 function App() {
+  const [isAuthenticated, userHasAuthenticated] = useState(false);
+  const [isAuthenticating, setIsAuthenticating] = useState(true);
+
+  useEffect(() => {
+    onLoad();
+  }, []);
+  
+  async function onLoad() {
+    try {
+      await Auth.currentSession();
+      userHasAuthenticated(true);
+    }
+    catch(e) {
+      if (e !== 'No current user') {
+        alert(e);
+      }
+    }
+  
+    setIsAuthenticating(false);
+  }
+  
+  function handleLogout() {
+    userHasAuthenticated(false);
+  }
+
   return (
+    !isAuthenticating &&
     <div className="App container">
       <Navbar fluid collapseOnSelect>
         <Navbar.Header>
           <Navbar.Brand>
-            <Link to="/">Simple Post-it</Link>
+            <Link to="/">Scratch</Link>
           </Navbar.Brand>
           <Navbar.Toggle />
         </Navbar.Header>
         <Navbar.Collapse>
           <Nav pullRight>
-            <LinkContainer to="/signup">
-              <NavItem>Signup</NavItem>
-            </LinkContainer>
-            <LinkContainer to="/login">
-              <NavItem>Login</NavItem>
-            </LinkContainer>
+            {isAuthenticated
+              ? <NavItem onClick={handleLogout}>Logout</NavItem>
+              : <>
+                  <LinkContainer to="/signup">
+                    <NavItem>Signup</NavItem>
+                  </LinkContainer>
+                  <LinkContainer to="/login">
+                    <NavItem>Login</NavItem>
+                  </LinkContainer>
+                </>
+            }
           </Nav>
         </Navbar.Collapse>
       </Navbar>
-      <Routes />
+      <AppContext.Provider
+        value={{ isAuthenticated, userHasAuthenticated }}
+      >
+        <Routes />
+      </AppContext.Provider>
     </div>
   );
 }
--- src/config.js
+++ src/config.js
@@ -9,7 +9,7 @@ export default {
   },
   cognito: {
     REGION: "us-east-2",
-    USER_POOL_ID: "us-east-2_mYYRI6lv6",
+    USER_POOL_ID: "us-east-2_XoZObaBjQ",
     APP_CLIENT_ID: "7m3panhclvv41vbi6h4cnqa0q2",
     IDENTITY_POOL_ID: "us-east-2:672ab073-4454-4506-816d-9276ab0b48d4"
   }
