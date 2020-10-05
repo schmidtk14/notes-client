3df9c83e5b4b4b25bafbfbf8caf1bc2cf1291483
Kevin Schmidt <schmidtk14@gmail.com>
2020-10-05 08:50:09 -0400

    Signup page created and added to Routes.js
    -It is a simple layout form but generates 2 confirmations conditionally
     based on if a user object is present
    -the two froms are validateForm and validateConfirmationForm
    -for now handleSubmit and handConfirmationSubmit just sett isLoading state
     and a temporary value for the newUser state

--- src/Routes.js
+++ src/Routes.js
@@ -3,6 +3,7 @@ import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home";
 import NotFound from "./containers/NotFound";
 import Login from "./containers/Login";
+import Signup from "./containers/Signup";
 
 
 
@@ -14,6 +15,9 @@ export default function Routes() {
       </Route>
       <Route exact path="/login">
         <Login />
+      </Route>
+      <Route exact path="/signup">
+        <Signup />
       </Route>
       <Route>
         <NotFound />
