8c84496951290ba223ea995bd88a61d666a0b7af
Kevin Schmidt <schmidtk14@gmail.com>
2020-10-05 10:42:40 -0400

    Notes.js created for rendering existing note by id
    -route added to Routes.js to route to notes/id
    -Notes.css added for simple styling

--- src/Routes.js
+++ src/Routes.js
@@ -5,6 +5,7 @@ import NotFound from "./containers/NotFound";
 import Login from "./containers/Login";
 import Signup from "./containers/Signup";
 import NewNote from "./containers/NewNote";
+import Notes from "./containers/Notes";
 
 
 
@@ -22,6 +23,9 @@ export default function Routes() {
       </Route>
       <Route exact path="/notes/new">
         <NewNote />
+      </Route>
+      <Route exact path="/notes/:id">
+        <Notes />
       </Route>
       <Route>
         <NotFound />
