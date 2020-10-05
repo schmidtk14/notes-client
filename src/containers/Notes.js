5b63268ab1b96e9016ccd332f7ad1467b2db6bef
Kevin Schmidt <schmidtk14@gmail.com>
2020-10-05 12:07:26 -0400

    Notes.js updated, delete button now deletes note from backend with aws amplify

--- src/containers/Notes.js
+++ src/containers/Notes.js
@@ -80,6 +80,10 @@ export default function Notes() {
     }
   }
   
+  function deleteNote() {
+    return API.del("notes", `/notes/${id}`);
+  }
+  
   async function handleDelete(event) {
     event.preventDefault();
   
@@ -92,6 +96,14 @@ export default function Notes() {
     }
   
     setIsDeleting(true);
+  
+    try {
+      await deleteNote();
+      history.push("/");
+    } catch (e) {
+      onError(e);
+      setIsDeleting(false);
+    }
   }
   
   return (
