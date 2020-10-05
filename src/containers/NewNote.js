5ae69c0a7664dfeb3ed8c6eee53f5d3efd3a4ade
Kevin Schmidt <schmidtk14@gmail.com>
2020-10-05 10:28:57 -0400

    NewNote.js added for creating new note and linked with api with AWS

---
+++ src/containers/NewNote.js
@@ -0,0 +1,62 @@
+import React, { useRef, useState } from "react";
+import { useHistory } from "react-router-dom";
+import { FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+import LoaderButton from "../components/LoaderButton";
+import { onError } from "../libs/errorLib";
+import config from "../config";
+import "./NewNote.css";
+import { API } from "aws-amplify";
+
+export default function NewNote() {
+;
+  const history = useHistory();
+  const [content, setContent] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  function validateForm() {
+    return content.length > 0;
+  }
+  async function handleSubmit(event) {
+    event.preventDefault();  
+    setIsLoading(true);
+  
+    try {
+      await createNote({ content });
+      history.push("/");
+    } catch (e) {
+      onError(e);
+      setIsLoading(false);
+    }
+  }
+  
+  function createNote(note) {
+    return API.post("notes", "/notes", {
+      body: note
+    });
+  }
+
+  return (
+    <div className="NewNote">
+      <form onSubmit={handleSubmit}>
+        <FormGroup controlId="content">
+          <FormControl
+            value={content}
+            componentClass="textarea"
+            onChange={e => setContent(e.target.value)}
+          />
+        </FormGroup>
+
+        <LoaderButton
+          block
+          type="submit"
+          bsSize="large"
+          bsStyle="primary"
+          isLoading={isLoading}
+          disabled={!validateForm()}
+        >
+          Create
+        </LoaderButton>
+      </form>
+    </div>
+  );
+}
